1a1b18c37e99403e5da8cc3947a65bda5331bad8
Fran Romero <73040385+FranRomerovedat@users.noreply.github.com>
2020-10-19 14:57:53 +0100

    Update pdfcreator.py

--- pdfcreator.py
+++ pdfcreator.py
@@ -5,20 +5,21 @@ from reportlab.lib.pagesizes import letter
 
 names = ['name1', 'name2', 'name3']
 
-packet = io.BytesIO()
-# create a new PDF with Reportlab
-can = canvas.Canvas(packet, pagesize=letter)
-can.setFont("Helvetica", 25)
-can.setFillColorRGB(255, 255, 255)
-can.drawString(650, 118, "Fran Romero")
-can.save()
+for name in names:
+    packet = io.BytesIO()
+    # create a new PDF with Reportlab
+    can = canvas.Canvas(packet, pagesize=letter)
+    can.setFont("Helvetica", 25)
+    can.setFillColorRGB(255, 255, 255)
+    can.drawString(650, 118, name)
+    can.save()
 
 #move to the beginning of the StringIO buffer
 packet.seek(0)
 new_pdf = PdfFileReader(packet)
 
 # read your existing PDF
-existing_pdf = PdfFileReader(open('filelocation', "rb"))
+existing_pdf = PdfFileReader(open('/Users/file.pdf', "rb"))
 output = PdfFileWriter()
 
 # add the "watermark" (which is the new pdf) on the existing page
